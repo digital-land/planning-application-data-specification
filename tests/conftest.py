022e7018e9d29b574854b9ca2e86b01e0ba183db
Adam Shimali <adam.shimali@gmail.com>
2025-09-19 13:19:11 +0100

    tool: Parameterize json schema validation unit test test_valid_application

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,15 +11,6 @@ PROJECT_ROOT = Path(__file__).parent.parent
 sys.path.insert(0, str(PROJECT_ROOT / "bin"))
 
 
-@pytest.fixture
-def hh_application_payload():
-    path = PROJECT_ROOT / "specification" / "example" / "example-hh-payload-minimal.json"
-    with open(path) as f:
-        return json.load(f)
-
-
-@pytest.fixture
-def hh_json_schema():
-    path = PROJECT_ROOT / "generated" / "json-schema" / "applications" / "hh.json"
-    with open(path) as f:
-        return json.load(f)
+@pytest.fixture()
+def project_root():
+    return PROJECT_ROOT
--- tests/test_json_schemas.py
+++ tests/test_json_schemas.py
@@ -1,10 +1,50 @@
 import pytest
+import json
 import jsonschema
 
 
-def test_valid_householder_application(hh_application_payload, hh_json_schema):
+@pytest.fixture
+def json_dir(project_root):
+    path = project_root / "specification" / "example"
+    return path
+
+@pytest.fixture
+def schema_dir(project_root):
+    path = project_root / "generated" / "json-schema" / "applications"
+    return path
+
+'''
+Add test files to the test_data list. First item of tuple is a filename
+of a json payload that should be in the specification/examples directory.
+The second item should be the corresponding json schema for the application
+type in generated/json-schema/application
+
+for example:
+
+test_data = [
+    ("example-hh-payload-minimal.json", "hh.json"),
+    ("example-full.json", "full.json"),
+    ("example-outline.json", "outline.json")
+]
+'''
+
+
+test_data = [
+    ("example-hh-payload-minimal.json", "hh.json")
+]
+
+@pytest.mark.parametrize("payload, schema", test_data)
+def test_valid_application(payload, schema, json_dir, schema_dir):
+    application_json = _load_json(json_dir, payload)
+    application_json_schema = _load_json(schema_dir, schema)
     try:
-        validator = jsonschema.Draft7Validator(hh_json_schema)
-        validator.validate(hh_application_payload)
+        validator = jsonschema.Draft7Validator(application_json_schema)
+        validator.validate(application_json)
     except jsonschema.exceptions.ValidationError as e:
         pytest.fail(f"Validation failed: {e}")
+
+
+def _load_json(dir, filename):
+    path = dir / filename
+    with open(path, "r") as f:
+        return json.load(f)
